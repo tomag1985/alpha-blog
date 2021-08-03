501d2a6f94132bc3b5e23e1add4eada3e1d1289e
tomas agnese <tagnese1985@gmail.com>
2021-08-03 14:35:17 -0300

    crud actions completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,38 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article saved!"
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article updated!"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
 end
