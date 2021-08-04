4a5463d6eaa6d0870330dd4d7fb75e4b613e1e2d
tomas agnese <tagnese1985@gmail.com>
2021-08-04 15:38:43 -0300

    add editing user options (controller and view)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-	def new
+	before_action :set_user, only:[:edit, :update]
+
+  def new
     @user = User.new
 	end
 
@@ -13,10 +15,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+		if @user.update(user_params)
+			flash[:notice] = "User info updated!"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+
   private
 
   def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
+
+  def set_user
+		@user = User.find(params[:id])
+  end
 
 end
