54517633772c6b6d2969527944d514235e0da64c
tomas agnese <tagnese1985@gmail.com>
2021-08-04 11:03:33 -0300

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+	validates :username, presence: true, length: { minimum:3, maximum:25 }, uniqueness: { case_sensitive: false }
+	validates :email, presence: true, length: { maximum:105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
