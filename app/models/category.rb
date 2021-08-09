1cdb6dc32fbca2a03f71df24639792ff01e6e36a
tomas agnese <tagnese1985@gmail.com>
2021-08-09 17:58:12 -0300

    many to many association finished

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:5, maximum:20}
 end
